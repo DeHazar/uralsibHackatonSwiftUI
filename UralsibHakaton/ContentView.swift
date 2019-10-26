f1bdbef8d78df06f2aad3ff3922486b2103aa7e8
Гараев Денис <garaev.denchik@gmail.com>
2019-10-26 12:32:51 +0500

    Initial Commit

---
+++ UralsibHakaton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UralsibHakaton
+//
+//  Created by Denchik on 26.10.2019.
+//  Copyright © 2019 bessovestniy-design. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
